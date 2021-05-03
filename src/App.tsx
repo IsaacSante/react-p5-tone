411b9d3a7ef0cf208ea331d5cb23bd406584b3b4
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2021-05-03 19:28:43 -0400

    commited

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,62 @@ import React, { useState, useEffect,  useRef }  from 'react';
 import './App.css';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketches/sketch';
-import { Button, Paper } from '@material-ui/core';
+import { Button, Paper, Slider } from '@material-ui/core';
 import * as Tone from 'tone'
 const file = require('./sketches/audiofiles/Stronger.mp3');
 
 function App() {
   const [diameter, setDiameter] = useState(100)
   const [red, setRed] = useState(100)
+  // const [volumeVal, setVolumeValue] = useState(0);
+  const [fxAmount, fxSetValue] = useState(0);
+  const [myCoolPlayer, sendPlayer] = useState({});
+  // const [analyzeVals, setAnalyzeVals] = useState(0);
   const [isLoaded, setIsLoading] = useState(false)
   const [isPlaying, setIsPlaying] = useState(true)
   const [btnTxt, buttonContext] = useState("Play")
 
-  let player = new Tone.Player({
+  const player = new Tone.GrainPlayer({
       url:file,
       loop:false,
       onload: () => setIsLoading(true)
     }).toDestination()
 
+
+    const meter = new Tone.Meter();
+    player.connect(meter)
     const playerRef = useRef(player);
 
-    function playSong(){
+    useEffect(() => {
+      // playerRef.current.volume.value = volumeVal;
+      playerRef.current.detune = fxAmount;
+    }, [fxAmount]);
+
+    // console.log(meter)
+
+     useEffect(() => {
+      if(isLoaded && !isPlaying){
+      sendPlayer(myCoolPlayer => meter)
+      }else if(isLoaded && isPlaying){
+        sendPlayer(0)
+      }else{
+        sendPlayer(0)
+      }
+     }, [isLoaded, isPlaying, myCoolPlayer, meter]);
+
+
+
+  // }, [volumeVal, fxAmount, myCoolPlayer, meter]);
+
+    // useEffect(() => {
+    //   const interval = setInterval(() => {
+    //     setAnalyzeVals(analyzeVals =>  meter.getValue());
+    //   }, 1000);
+    //   return () => clearInterval(interval);
+    // }, []);
+
+
+  function playSong(){
        if(isLoaded === true){
          if(isPlaying===true){
          buttonContext("Stop")
@@ -36,8 +72,6 @@ function App() {
     }
 
 
-
-
   return (
           <div className = "App">
                 <div className="site">
@@ -45,18 +79,15 @@ function App() {
                               <h1>MSV17</h1>
                             </div>
                             <div className="App-cont">
-
                   <div className="Visuals">
                     <Paper elevation={3} >
-                      <P5Wrapper sketch={sketch} diameter={diameter} red={red} isLoaded={isLoaded} />
+                      <P5Wrapper sketch={sketch} diameter={diameter} red={red} isLoaded={isLoaded} myCoolPlayer={myCoolPlayer}/>
                     </Paper>
                   </div>
-
                   <div className="Controls">
                       <Button className="btn btn-default" disabled={!isLoaded} onClick={playSong}>
                         <p>{btnTxt}</p>
                       </Button>
-                      
                         <label htmlFor="slider">
                               <input
                                   id="slider1"
@@ -69,7 +100,6 @@ function App() {
                               />
                               {diameter}
                           </label>
-
                         <label htmlFor="slider">
                               <input
                                   id="slider2"
@@ -81,6 +111,32 @@ function App() {
                                   onChange={event => setRed(+event.target.value)}
                               />
                               {red}
+                          </label>
+{/* 
+                          <label htmlFor="slider">
+                              <input
+                                  id="volume"
+                                  type="range"
+                                  min={-20}
+                                  max={12}
+                                  step={0.1}
+                                  value={volumeVal}
+                                  onChange={event => setVolumeValue(+event.target.value)}
+                              />
+                              {volumeVal}
+                          </label> */}
+
+                          <label htmlFor="slider">
+                              <input
+                                  id="fxAmount"
+                                  type="range"
+                                  min={0}
+                                  max={200}
+                                  step={10}
+                                  value={fxAmount}
+                                  onChange={event => fxSetValue(+event.target.value)}
+                              />
+                              {fxAmount}
                           </label>
 
                     </div>
--- src/sketches/sketch.tsx
+++ src/sketches/sketch.tsx
@@ -1,31 +1,34 @@
+
+
 export default function sketch(p){
     let canvas;
     let diameter = 100;
     let red = 100;
     let w = window.innerWidth/2.5;
+    let playerObj = 0; 
+    let analyser;
 
     p.myCustomRedrawAccordingToNewPropsHandler = (Props) => {
       if(canvas) {
-        diameter = Props.diameter
-        red = Props.red
+        // diameter = Props.diameter
+        // red = Props.red
+        playerObj = Props.myCoolPlayer; 
+        // analyser = Props.analyzeVals
       }
     }
-
     p.setup = async () => {
       canvas = p.createCanvas(w, w);
       p.noStroke();
-
     }
 
-    p.draw = () => {
-        p.background(255, 1);
-        p.fill(100, 200, 100, red)
-        p.ellipse(canvas.width / 2 , canvas.width / 2, diameter * 2);
+      p.draw = () => {
+        if(playerObj === 0 || playerObj === 2)
+        return
+        console.log(playerObj)
+        p.background(255,0,100);
+        p.fill(100, 100, 100, 255)
+        p.ellipse(canvas.width / 2 , canvas.width / 2, 100 );
    }
 
- 
+
 }
-
-  
-  
-
